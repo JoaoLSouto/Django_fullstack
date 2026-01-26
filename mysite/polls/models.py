6b6c7cec5c72386e9b431045cd4393476b072970
joaolsouto <contato.joaolsouto@gmail.com>
2026-01-26 19:32:51 -0300

    Add custom admin site and update settings for localization

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,13 +2,21 @@ from django.db import models
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField("Texto da questão", max_length=200)
     pub_date = models.DateTimeField("date published")
     active = models.BooleanField(default=True)
     x = models.TextField()
+
+    class Meta:
+        verbose_name = "Questão"
+        verbose_name_plural = "Questões"
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "Opção"
+        verbose_name_plural = "Opções"
