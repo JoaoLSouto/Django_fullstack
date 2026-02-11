d59883c9ce157fd4fdf05e52725669ab57cd3099
joaolsouto <contato.joaolsouto@gmail.com>
2026-02-11 11:15:43 -0300

    Refactor settings module paths to include project name for consistency

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -45,7 +45,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "mysite.urls"
+ROOT_URLCONF = "mysite.mysite.urls"
 
 TEMPLATES = [
     {
@@ -62,7 +62,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "mysite.wsgi.application"
+WSGI_APPLICATION = "mysite.mysite.wsgi.application"
 
 
 # Database
